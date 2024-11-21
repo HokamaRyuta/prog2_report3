c813688d3fabc212cf60bfa6a23d31c27ade659a
Ryuta Hokama <e245703@ie.u-ryukyu.ac.jp>
2024-11-21 23:56:34 +0900

    EnemyTest.javaの再修正

--- app/src/test/java/jp/ac/uryukyu/ie/e245703/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e245703/EnemyTest.java
@@ -17,10 +17,10 @@ class EnemyTest {
         int defaultHeroHp = 100;
         Hero demoHero = new Hero("デモ勇者", defaultHeroHp, 100);
         Enemy slime = new Enemy("スライムもどき", 10, 100);
-        demoHero.attack(slime);
-        slime.attack(demoHero);
-        if(slime.isDead() == true){
-            assertEquals(defaultHeroHp, demoHero.getHitPoint());
+        while(!slime.isDead()){
+            demoHero.attack(slime);
         }
+        slime.attack(demoHero);
+        assertEquals(defaultHeroHp, demoHero.getHitPoint());
     }
 }
